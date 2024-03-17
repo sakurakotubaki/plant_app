c39341772fceca8249ac24945283b7cde4fe0e01
Jboy422 <gitonly543@gmail.com>
2024-03-17 13:14:21 +0900

    doc: UI/UX Design

---
+++ README.md
@@ -0,0 +1,4 @@
+# plant_app
+海外のUI/UXデザインのトレース。３年前のコードなのでリファクタリングが必要。
+
+https://www.youtube.com/watch?v=LN668OAUrK4&t=1732s
---
+++ android/app/src/main/kotlin/com/jboy/plant_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jboy.plant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
